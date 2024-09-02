1566267b5175052bcb93bf3560de1e5279a340c4
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-09-02 15:08:10 +0600

    adding max-number of the given array

--- task-7.js
+++ task-7.js
@@ -4,7 +4,15 @@
 
 const numbers = [12,4,8,90,7,6,89,9];
 
+let maxNumber = numbers[0];
+
 
 for(let i = 0;i<numbers.length;i++){
-    console.log(numbers[i]);
+    // console.log(Math.max(numbers[i]));
+    // console.log(numbers[i]);
+    if(numbers[i]>maxNumber){
+        maxNumber = numbers[i]
+    }
+
 }
+console.log(maxNumber);
